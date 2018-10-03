de52bc7070ba5efa05f46ed61c141fd355d596a1
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-03 15:25:47 +0100

    added full CRUD for nested comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   has_many :blogs
+  has_many :comments
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
+
+  def full_name
+    "#{first_name} #{last_name}"
+    
+  end
 
 end
