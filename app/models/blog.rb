01de4469bd3dde3b17fe08fcff1deec1a8666014
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-03 21:51:40 +0100

    validations added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,9 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :user_id, :presence => true
   validates :title, :presence => true
   validates :body, :presence => true
+  validates :body, length: {maximum: 300}
   validates :image, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :first_name, :presence => true
+  validates :first_name, :format => {
+    :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ :message => 'Please enter letters only'
+  }
+
   validates :last_name, :presence => true
+  validates :last_name, :format => {
+    :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ :message => 'Please enter letters only'
+  }
 
   def full_name
     "#{first_name} #{last_name}"
