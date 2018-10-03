e78131d24799895bd6c91c5555c51b9b0a36c904
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-03 13:46:22 +0100

    added simple validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
+
+  validates :title, :presence => true
+  validates :body, :presence => true
+  validates :image, :presence => true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :blogs
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+
 end
