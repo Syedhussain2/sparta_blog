934ac3c24ef32038ab75015882f090bf90ac16d9
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-10-03 18:15:17 +0100

    validations complete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
   belongs_to :user
-
+  validates :body, length: (maximum: 250)
   validates :title, :presence => true
   validates :body, :presence => true
   validates :image, :presence => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :blogs
-
+  validates :first_name, :format => {
+    :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
+  }
+  validates :last_name, :format => {
+    :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
+  }
   validates :first_name, :presence => true
   validates :last_name, :presence => true
 
